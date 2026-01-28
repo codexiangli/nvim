1c6c0d70b4243bb9a1bb4a5d7e4b9edd86e57031
lixiang138940 <xiang.li0701@atrenew.com>
2026-01-28 20:05:15 +0800

    插件better-escape

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -192,7 +192,7 @@ require("lazy").setup({
         config = function()
             require("lualine").setup({
                 options = {
-                    theme = "onedark",
+                    theme = "auto",
                     component_separators = { left = '', right = '' },
                     section_separators = { left = '', right = '' },
                 },
@@ -303,9 +303,9 @@ require("lazy").setup({
                     untracked = { text = '┆' },
                 },
                 signs_staged_enable = true,
-                signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-                numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-                linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+                signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+                numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+                linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
                 word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
                 watch_gitdir = {
                     follow_files = true
@@ -324,7 +324,7 @@ require("lazy").setup({
                 current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
                 sign_priority = 6,
                 update_debounce = 100,
-                status_formatter = nil,   -- Use default
+                status_formatter = nil,  -- Use default
                 max_file_length = 40000, -- Disable if file is longer than this (in lines)
                 preview_config = {
                     -- Options passed to nvim_open_win
@@ -599,4 +599,85 @@ require("lazy").setup({
             { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
+            },
+        },
+    },
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+
+    -- 使用 lazy.nvim 安装
+    {
+        'jdhao/better-escape.nvim',
+        event = 'InsertEnter',
+        config = function()
+            require('better_escape').setup({
+                mapping = { 'jk', 'kj' }, -- 多个映射
+                timeout = 300,   -- 超时时间
+                clear_empty_lines = false, -- 是否清除空行
+                keys = '<Esc>',  -- 要发送的键
+            })
+        end,
+    },
+
 })
