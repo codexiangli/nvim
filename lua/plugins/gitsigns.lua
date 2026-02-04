39f54237d3118c43763c517cf0b32a307405159d
lixiang138940 <xiang.li0701@atrenew.com>
2026-02-04 20:03:15 +0800

    新引入插件&插件修改
    1.修改gitsigns & 加入snacks troggle
    2.新引入插件：muliticursor、smartyank、uodotree、vim-waittime

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,57 +1,104 @@
 return {
-    -- ================  版本控制工具 =============
     {
         "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup {
-                signs = {
-                    add = { text = '┃' },
-                    change = { text = '┃' },
-                    delete = { text = '_' },
-                    topdelete = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked = { text = '┆' },
-                },
-                signs_staged = {
-                    add = { text = '┃' },
-                    change = { text = '┃' },
-                    delete = { text = '_' },
-                    topdelete = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked = { text = '┆' },
-                },
-                signs_staged_enable = true,
-                signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-                numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-                linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
-                word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-                watch_gitdir = {
-                    follow_files = true
-                },
-                auto_attach = true,
-                attach_to_untracked = false,
-                current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame_opts = {
-                    virt_text = true,
-                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                    delay = 1000,
-                    ignore_whitespace = false,
-                    virt_text_priority = 100,
-                    use_focus = true,
-                },
-                current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-                sign_priority = 6,
-                update_debounce = 100,
-                status_formatter = nil,  -- Use default
-                max_file_length = 40000, -- Disable if file is longer than this (in lines)
-                preview_config = {
-                    -- Options passed to nvim_open_win
-                    style = 'minimal',
-                    relative = 'cursor',
-                    row = 0,
-                    col = 1
-                },
-            }
-        end
+        event = "BufReadPost",
+        opts = {
+            signcolumn = false,
+            numhl = true,
+            -- word_diff = true,
+            current_line_blame = true,
+            attach_to_untracked = true,
+            preview_config = {
+                border = "rounded",
+            },
+            on_attach = function(bufnr)
+                local gitsigns = require("gitsigns")
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                -- stylua: ignore
+                map("n", "]h",
+                    function() if vim.wo.diff then vim.cmd.normal({ "]h", bang = true }) else gitsigns.nav_hunk("next") end end,
+                    { desc = "[Git] Next hunk" })
+                -- stylua: ignore
+                map("n", "]H",
+                    function() if vim.wo.diff then vim.cmd.normal({ "]H", bang = true }) else gitsigns.nav_hunk("last") end end,
+                    { desc = "[Git] Last hunk" })
+                -- stylua: ignore
+                map("n", "[h",
+                    function() if vim.wo.diff then vim.cmd.normal({ "[h", bang = true }) else gitsigns.nav_hunk("prev") end end,
+                    { desc = "[Git] Prev hunk" })
+                -- stylua: ignore
+                map("n", "[H",
+                    function() if vim.wo.diff then vim.cmd.normal({ "[H", bang = true }) else gitsigns.nav_hunk("first") end end,
+                    { desc = "[Git] First hunk" })
+
+                -- Actions
+                map("n", "<leader>ggs", gitsigns.stage_hunk, { desc = "[Git] Stage hunk" })
+                -- stylua: ignore
+                map("v", "<leader>ggs", function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+                    { desc = "[Git] Stage hunk (Visual)" })
+
+                map("n", "<leader>ggr", gitsigns.reset_hunk, { desc = "[Git] Reset hunk" })
+                -- stylua: ignore
+                map("v", "<leader>ggr", function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end,
+                    { desc = "[Git] Reset hunk (Visual)" })
+
+                map("n", "<leader>ggS", gitsigns.stage_buffer, { desc = "[Git] Stage buffer" })
+                map("n", "<leader>ggR", gitsigns.reset_buffer, { desc = "[Git] Reset buffer" })
+
+                map("n", "<leader>ggp", gitsigns.preview_hunk, { desc = "[Git] Preview hunk" })
+                map("n", "<leader>ggP", gitsigns.preview_hunk_inline, { desc = "[Git] Preview hunk inline" })
+
+                -- map("n", "<leader>ggb", function() gitsigns.blame_line({ full = true }) end, { desc = "[Git] Blame line" })
+
+                -- stylua: ignore
+                -- map("n", "<leader>ggd", gitsigns.diffthis, { desc = "[Git] diff" })
+                -- stylua: ignore
+                -- map("n", "<leader>ggD", function() gitsigns.diffthis("~") end, { desc = "[Git] diff (ALL)" })
+
+                -- stylua: ignore
+                map("n", "<leader>ggQ", function() gitsigns.setqflist("all") end,
+                    { desc = "[Git] Show diffs (ALL) in qflist" })
+                -- stylua: ignore
+                map("n", "<leader>ggq", gitsigns.setqflist, { desc = "[Git] Show diffs in qflist" })
+
+                -- Text object
+                map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "[Git] Current hunk" })
+
+                -- Toggles
+                require("snacks")
+                    .toggle({
+                        name = "line blame",
+                        get = function()
+                            return require("gitsigns.config").config.current_line_blame
+                        end,
+                        set = function(enabled)
+                            require("gitsigns").toggle_current_line_blame(enabled)
+                        end,
+                    })
+                    :map("<leader>tgb")
+                require("snacks")
+                    .toggle({
+                        name = "word diff",
+                        get = function()
+                            return require("gitsigns.config").config.word_diff
+                        end,
+                        set = function(enabled)
+                            require("gitsigns").toggle_word_diff(enabled)
+                        end,
+                    })
+                    :map("<leader>tgw")
+            end,
+        },
+        config = function(_, opts)
+            require("gitsigns").setup(opts)
+            require("scrollbar.handlers.gitsigns").setup()
+        end,
     },
 }
